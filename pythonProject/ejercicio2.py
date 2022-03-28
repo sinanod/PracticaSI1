1cb22f92c157174737c03bb2c3a1c99e3ca333c0
sinanod <76814770+sinanod@users.noreply.github.com>
2022-03-28 23:24:05 +0200

    Act

--- pythonProject/ejercicio2.py
+++ pythonProject/ejercicio2.py
@@ -3,10 +3,6 @@ import pandas as pd;
 
 con = sqlite3.connect('SISTINF.db');
 
-query = con.execute("SELECT * From legal")
-cols = [column[0] for column in query.description]
-dLegal = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
-
 query = con.execute("SELECT * From users")
 cols = [column[0] for column in query.description]
 dUsers = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
@@ -15,17 +11,25 @@ query = con.execute("SELECT * From fechas_user")
 cols = [column[0] for column in query.description]
 dFechas_User = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
-query = con.execute("SELECT * From ips_user")
+query = con.execute("SELECT count(ip) as numAccesos From ips_user group by rtb_user")
 cols = [column[0] for column in query.description]
 dIPS_User = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+
+query = con.execute("select count(fecha) as numAccesos from fechas_user group by rtb_user;")
+cols = [column[0] for column in query.description]
+dFechas_User = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
 con.close()
 
 
-print("La media de correos recibidos son: ", dUsers['emailsPhising'].describe()['mean'])
-print("La desviacion estándar de correos recibidos son: ", dUsers['emailsPhising'].describe()['std'])
-print("El valor máximo de correos recibidos es: ", dUsers['emailsTotal'].describe()['max'])
-print("La valor mínimo de correos recibidos es: ", dUsers['emailsTotal'].describe()['min'])
+print("La media de correos recibidos son:", dUsers['emailsPhising'].mean())
+print("La desviacion estándar de correos recibidos son: ", dUsers['emailsPhising'].std())
+print("El valor máximo de correos recibidos es:", dUsers['emailsTotal'].max())
+print("La valor mínimo de correos recibidos es:", dUsers['emailsTotal'].min())
 
-print("El valor máximo de las fechas que se ha iniciado sesión es: ", dFechas_User['fecha'].describe()['top'])
 
+print("El valor máximo de las fechas que se ha iniciado sesión es:", dFechas_User['numAccesos'].max())
+print("El valor mínimo de las fechas que se ha iniciado sesión es:", dFechas_User['numAccesos'].min())
+
+print("La media de accesos por IP es de:", dIPS_User['numAccesos'].mean())
+print("La desviación estándar de accesos por IP es de:", dIPS_User['numAccesos'].std())
--- pythonProject/ejercicio3.py
+++ pythonProject/ejercicio3.py
@@ -3,30 +3,57 @@ import pandas as pd;
 
 con = sqlite3.connect('SISTINF.db');
 
-query = con.execute("SELECT * From legal")
+query = con.execute("SELECT * From users where permisos  = 0")
 cols = [column[0] for column in query.description]
-dLegal = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+dUsersUser = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
-query = con.execute("SELECT * From users")
+query = con.execute("SELECT * From users where permisos  = 1")
 cols = [column[0] for column in query.description]
-dUsers = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+dUsersAdmin = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
-query = con.execute("SELECT * From fechas_user")
-cols = [column[0] for column in query.description]
-dFechas_User = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
-query = con.execute("SELECT * From ips_user")
+query = con.execute("SELECT * From users where emailsPhising >= 200")
 cols = [column[0] for column in query.description]
-dIPS_User = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+dUsersMas200 = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+
+query = con.execute("SELECT * From users where emailsPhising < 200")
+cols = [column[0] for column in query.description]
+dUsersMenos200 = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+
 
 con.close()
 
+print("USUARIOS")
 
-print("El número de observaciones es:", dUsers['emailsPhising'].describe()['count'])
-print("La mediana es:", dUsers['emailsPhising'].median())
-print("La media es:",dUsers['emailsPhising'].describe()['mean'])
-print("La varianza es:", dUsers['emailsPhising'].var())
-print("El valor mínimo y máximos es:", dUsers['emailsPhising'].describe()['min'],
-      "y el valor máximo es", dUsers['emailsPhising'].describe()['max'])
+print("El número de observaciones es:", dUsersUser['emailsPhising'].count())
+print("La mediana es:", dUsersUser['emailsPhising'].median())
+print("La media es:",dUsersUser['emailsPhising'].mean())
+print("La varianza es:", dUsersUser['emailsPhising'].var())
+print("El valor mínimo y máximos es:", dUsersUser['emailsPhising'].min(),
+      "y el valor máximo es", dUsersUser['emailsPhising'].max())
+
+print("\nADMIN")
+print("El número de observaciones es:", dUsersAdmin['emailsPhising'].count())
+print("La mediana es:", dUsersAdmin['emailsPhising'].median())
+print("La media es:",dUsersAdmin['emailsPhising'].mean())
+print("La varianza es:", dUsersAdmin['emailsPhising'].var())
+print("El valor mínimo y máximos es:", dUsersAdmin['emailsPhising'].min(),
+      "y el valor máximo es", dUsersAdmin['emailsPhising'].max())
 
 
+print("\nMENOS DE 200 CORREOS")
+print("El número de observaciones es:", dUsersMenos200['emailsPhising'].count())
+print("La mediana es:", dUsersMenos200['emailsPhising'].median())
+print("La media es:",dUsersMenos200['emailsPhising'].mean())
+print("La varianza es:", dUsersMenos200['emailsPhising'].var())
+print("El valor mínimo y máximos es:", dUsersMenos200['emailsPhising'].min(),
+      "y el valor máximo es", dUsersMenos200['emailsPhising'].max())
+
+
+print("\nMAS DE 200 CORREOS")
+print("El número de observaciones es:", dUsersMas200['emailsPhising'].count())
+print("La mediana es:", dUsersMas200['emailsPhising'].median())
+print("La media es:",dUsersMas200['emailsPhising'].mean())
+print("La varianza es:", dUsersMas200['emailsPhising'].var())
+print("El valor mínimo y máximos es:", dUsersMas200['emailsPhising'].min(),
+      "y el valor máximo es", dUsersMas200['emailsPhising'].max())
